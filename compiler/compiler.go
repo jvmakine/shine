6720539af5f61f9442735f85aeee205add924021
Juho Makinen <jvmakine@gmail.com>
2020-02-27 00:54:03 +0000

    Code generation for addition

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,31 +1,38 @@
 package compiler
 
 import (
-	"strconv"
-
 	"github.com/jvmakine/shine/grammar"
 	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
+	"github.com/llir/llvm/ir/value"
 )
 
-func add(exp *grammar.Expression) int {
-	if exp.Add != nil {
-		return *exp.Value + add(exp.Add)
+func expression(module *ir.Module, block *ir.Block, exp *grammar.Expression) value.Value {
+	e := exp
+	var v value.Value
+	v = constant.NewInt(types.I32, int64(*exp.Value))
+
+	for e.Add != nil {
+		v = block.NewAdd(v, constant.NewInt(types.I32, int64(*e.Add.Value)))
+		e = e.Add
 	}
-	return *exp.Value
+
+	return v
 }
 
 func Compile(prg *grammar.Program) *ir.Module {
 	module := ir.NewModule()
-	str := strconv.Itoa(add(prg.Exp))
-	msg := module.NewGlobalDef("msg", constant.NewCharArrayFromString(str))
-	puts := module.NewFunc("puts", types.I32, ir.NewParam("msg", types.I8Ptr))
+
+	msg := module.NewGlobalDef("intFormat", constant.NewCharArrayFromString("%d\n"))
+	printf := module.NewFunc("printf", types.I32, ir.NewParam("msg", types.I8Ptr))
+	printf.Sig.Variadic = true
 
 	mainfun := module.NewFunc("main", types.I32)
 	entry := mainfun.NewBlock("")
-	ptr := entry.NewGetElementPtr(types.NewArray(uint64(len(str)), types.I8), msg, constant.NewInt(types.I64, 0), constant.NewInt(types.I64, 0))
-	entry.NewCall(puts, ptr)
+	v := expression(module, entry, prg.Exp)
+	ptr := entry.NewGetElementPtr(types.NewArray(3, types.I8), msg, constant.NewInt(types.I64, 0), constant.NewInt(types.I64, 0))
+	entry.NewCall(printf, ptr, v)
 	entry.NewRet(constant.NewInt(types.I32, 0))
 	return module
 }
