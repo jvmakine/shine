2c8e3c1cc70e9a5f5a45301629bbd36d8d134baf
Juho Makinen <jvmakine@gmail.com>
2020-02-29 19:10:11 +0000

    Allow functions in any order

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -8,26 +8,36 @@ import (
 	"github.com/llir/llvm/ir/types"
 )
 
-func evalFunDef(module *ir.Module, fun *grammar.FunDef, ctx *context) error {
+func makeFunDefinition(module *ir.Module, fun *grammar.FunDef, ctx *context) error {
 	var params []*ir.Param
-	subCtx := ctx.subContext()
 	for _, p := range fun.Params {
 		param := ir.NewParam(*p.Name, types.I32)
-		subCtx.addId(*p.Name, param)
 		params = append(params, param)
 	}
 
 	compiled := module.NewFunc(*fun.Name, types.I32, params...)
 	compiled.Linkage = enum.LinkageInternal
 
-	body := compiled.NewBlock("")
-	result, err := evalExpression(body, fun.Body, subCtx)
-	if err != nil {
-		return err
-	}
-	body.NewRet(result)
-
 	ctx.addFun(*fun.Name, &compiledFun{fun, compiled})
+	return nil
+}
+
+func compileFunBodies(ctx *context) error {
+	for _, f := range ctx.functions {
+		body := f.Fun.NewBlock("")
+		subCtx := ctx.subContext()
+		var params []*ir.Param
+		for _, p := range f.From.Params {
+			param := ir.NewParam(*p.Name, types.I32)
+			subCtx.addId(*p.Name, param)
+			params = append(params, param)
+		}
+		result, err := evalExpression(body, f.From.Body, subCtx)
+		if err != nil {
+			return err
+		}
+		body.NewRet(result)
+	}
 	return nil
 }
 
@@ -43,11 +53,12 @@ func Compile(prg *grammar.Program) (*ir.Module, error) {
 
 	ctx := context{}
 	for _, f := range prg.Functions {
-		err := evalFunDef(module, f, &ctx)
+		err := makeFunDefinition(module, f, &ctx)
 		if err != nil {
 			return nil, err
 		}
 	}
+	compileFunBodies(&ctx)
 	v, err := evalExpression(entry, prg.Exp, &ctx)
 	if err != nil {
 		return nil, err
