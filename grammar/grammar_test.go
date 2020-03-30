e09d4f8a389f5013bfb043ffe32ec20b5a395608
Juho Makinen <jvmakine@gmail.com>
2020-03-30 18:37:10 +1100

    Unit tests for the parser

---
+++ grammar/grammar_test.go
@@ -0,0 +1,49 @@
+package grammar
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  *Block
+	}{{
+		name:  "parse a simple numeric expression",
+		input: "42",
+		want:  &Block{Value: &Expression{Term: &TermExpression{Left: intTerm(42)}}},
+	}, {
+		name:  "parse + expression",
+		input: "1 + 2",
+		want:  &Block{Value: &Expression{Term: binTerm("+", intTerm(1), intTerm(2))}},
+	}, {
+		name:  "parse - expression",
+		input: "1 - 2",
+		want:  &Block{Value: &Expression{Term: binTerm("-", intTerm(1), intTerm(2))}},
+	},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			prog, err := Parse(tt.input)
+			got := prog.Body
+			if err != nil {
+				t.Errorf("Parse() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() = %v, want %v", *got, *tt.want)
+			}
+		})
+	}
+}
+
+func binTerm(op string, left *Term, right *Term) *TermExpression {
+	return &TermExpression{Left: left, Right: []*OpTerm{&OpTerm{Operation: &op, Right: right}}}
+}
+
+func intTerm(i int) *Term {
+	integer := i
+	return &Term{Left: &Value{Int: &integer}}
+}
