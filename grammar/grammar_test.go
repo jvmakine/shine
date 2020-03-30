119ae786786482be4e61773005b6100b7036b923
Juho Makinen <jvmakine@gmail.com>
2020-03-30 19:51:25 +1100

    Unit tests for the parser

--- grammar/grammar_test.go
+++ grammar/grammar_test.go
@@ -15,13 +15,25 @@ func TestParse(t *testing.T) {
 		input: "42",
 		want:  &Block{Value: &Expression{Term: &TermExpression{Left: intTerm(42)}}},
 	}, {
-		name:  "parse + expression",
+		name:  "parse + term expression",
 		input: "1 + 2",
 		want:  &Block{Value: &Expression{Term: binTerm("+", intTerm(1), intTerm(2))}},
 	}, {
-		name:  "parse - expression",
+		name:  "parse - term expression",
 		input: "1 - 2",
 		want:  &Block{Value: &Expression{Term: binTerm("-", intTerm(1), intTerm(2))}},
+	}, {
+		name:  "parse * factor expression",
+		input: "2 * 3",
+		want:  &Block{Value: &Expression{Term: &TermExpression{Left: binFact("*", intVal(2), intVal(3))}}},
+	}, {
+		name:  "parse / factor expression",
+		input: "2 / 3",
+		want:  &Block{Value: &Expression{Term: &TermExpression{Left: binFact("/", intVal(2), intVal(3))}}},
+	}, {
+		name:  "maintain right precedence with + and *",
+		input: "2 + 3 * 4",
+		want:  &Block{Value: &Expression{Term: binTerm("+", intTerm(2), binFact("*", intVal(3), intVal(4)))}},
 	},
 	}
 	for _, tt := range tests {
@@ -39,11 +51,19 @@ func TestParse(t *testing.T) {
 	}
 }
 
+func binFact(op string, left *Value, right *Value) *Term {
+	return &Term{Left: left, Right: []*OpFactor{&OpFactor{Operation: &op, Right: right}}}
+}
+
 func binTerm(op string, left *Term, right *Term) *TermExpression {
 	return &TermExpression{Left: left, Right: []*OpTerm{&OpTerm{Operation: &op, Right: right}}}
 }
 
 func intTerm(i int) *Term {
+	return &Term{Left: intVal(i)}
+}
+
+func intVal(i int) *Value {
 	integer := i
-	return &Term{Left: &Value{Int: &integer}}
+	return &Value{Int: &integer}
 }
