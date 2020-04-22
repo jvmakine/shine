570318016f37646ad21ab7be60e9f49bff7ee743
Juho Makinen <jvmakine@gmail.com>
2020-04-22 19:17:05 +1000

    Function pointer types

--- compiler/types.go
+++ compiler/types.go
@@ -11,20 +11,35 @@ var (
 	RealType = types.Double
 )
 
+func getFunctPtr(fun t.Type) types.Type {
+	ret := getType(fun.FunctReturn())
+	fparams := fun.FunctParams()
+	params := make([]types.Type, len(fparams))
+	for i, p := range fparams {
+		params[i] = getType(p)
+	}
+	return types.NewPointer(types.NewFunc(ret, params...))
+}
+
 func getType(typ t.Type) types.Type {
-	var rtype types.Type = nil
-	if !typ.IsDefined() {
+	if !typ.IsDefined() && !typ.IsVariable() {
 		panic("trying to use undefined type at compilation")
 	}
-	switch typ.AsPrimitive() {
-	case t.Int:
-		rtype = IntType
-	case t.Bool:
-		rtype = BoolType
-	case t.Real:
-		rtype = RealType
-	default:
-		panic("unsupported type at compilation")
+	if typ.IsPrimitive() {
+		var rtype types.Type = nil
+		switch typ.AsPrimitive() {
+		case t.Int:
+			rtype = IntType
+		case t.Bool:
+			rtype = BoolType
+		case t.Real:
+			rtype = RealType
+		default:
+			panic("unsupported type at compilation")
+		}
+		return rtype
+	} else if typ.IsFunction() {
+		return getFunctPtr(typ)
 	}
-	return rtype
+	panic("invalid type: " + typ.Signature())
 }
