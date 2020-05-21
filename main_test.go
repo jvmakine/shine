5b7e234c66de8e60136d0946c007e3fd8ac57cc4
Juho Makinen <jvmakine@gmail.com>
2020-05-21 17:54:35 +1000

    Enable main tests

--- main_test.go
+++ main_test.go
@@ -10,9 +10,9 @@ func TestCompile(t *testing.T) {
 		name    string
 		program string
 		err     error
-	}{ /*{
-			name: "compiles functions as variables program without errors",
-			program: `
+	}{{
+		name: "compiles functions as variables program without errors",
+		program: `
 							operate = (x, y, f) => { f(x, y) }
 							add = (x, y) => { x + y }
 							sub = (x, y) => { x - y }
@@ -20,10 +20,10 @@ func TestCompile(t *testing.T) {
 
 							operate(3, 1, pick(true)) + operate(5, 1, pick(false)) + operate(1, 1, (x, y) => { x + y })
 						`,
-			err: nil,
-		}, {
-			name: "compile euler2 without errors",
-			program: `
+		err: nil,
+	}, {
+		name: "compile euler2 without errors",
+		program: `
 						agg = (p2, p1, u, sum) => {
 							cur = p1 + p2
 							if (cur > u) sum else {
@@ -33,10 +33,10 @@ func TestCompile(t *testing.T) {
 						}
 						agg(1, 1, 100, 0)
 					`,
-			err: nil,
-		}, {
-			name: "compile nested function args without errors",
-			program: `
+		err: nil,
+	}, {
+		name: "compile nested function args without errors",
+		program: `
 				findLargestProd = (start, stop, cond) => {
 					agg = (b, e, le, ri, max, c) => {
 						if (le > e) {
@@ -54,23 +54,23 @@ func TestCompile(t *testing.T) {
 
 				findLargestProd(10, 99, (x) => x % 2 == 0)
 			`,
-			err: nil,
-		},*/{
-			name: "compile sequential functions",
-			program: `
+		err: nil,
+	}, {
+		name: "compile sequential functions",
+		program: `
 			a = (x) => (y) => (z) => x + y + z
 			a(1)(2)(3)
 		`,
-			err: nil,
-		}, /* {
-			name: "cwork with unused values",
-			program: `
+		err: nil,
+	}, {
+		name: "cwork with unused values",
+		program: `
 				a = b
 				b = 3
 				3
 			`,
-			err: nil,
-		}*/}
+		err: nil,
+	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_, err := Compile(tt.program)
