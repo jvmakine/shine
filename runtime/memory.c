4eefbc68756d9b4e82f1b00ffa135bd69a3f4920
Juho Makinen <jvmakine@gmail.com>
2020-05-26 13:59:31 +1000

    Function to increase reference count

--- runtime/memory.c
+++ runtime/memory.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void free_closure(void *cls) {
+void free_reference(void *cls) {
     int32_t refcount = *((int32_t*)cls);
     if (refcount <= 1) {
         int16_t clscount = *((int16_t*)(((int32_t*)cls) + 1));
         int8_t* ptr = (int8_t*)((int16_t*)(((int32_t*)cls) + 1) + 1);
         while (clscount > 0) {
             ptr++;
-            free_closure(ptr);
+            free_reference(ptr);
             ptr++;
             clscount--;
         }
@@ -16,4 +16,9 @@ void free_closure(void *cls) {
     } else {
         *((int32_t*)cls) = refcount - 1;
     }
+}
+
+void increase_refcount(void *cls) {
+    int32_t refcount = *((int32_t*)cls);
+    *((int32_t*)cls) = refcount + 1;
 }
