07ba5b84857a01a1e475b1c3b1f9004f7cde08ce
Juho Makinen <jvmakine@gmail.com>
2020-05-28 15:31:49 +1000

    Colouring for primitive types

--- main_test.go
+++ main_test.go
@@ -14,47 +14,47 @@ func TestCompile(t *testing.T) {
 	}{{
 		name: "compiles functions as variables program without errors",
 		program: `
-							operate = (x, y, f) => { f(x, y) }
-							add = (x, y) => { x + y }
-							sub = (x, y) => { x - y }
-							pick = (b) => { if (b) sub else add }
+					operate = (x, y, f) => { f(x, y) }
+					add = (x, y) => { x + y }
+					sub = (x, y) => { x - y }
+					pick = (b) => { if (b) sub else add }
 
-							operate(3, 1, pick(true)) + operate(5, 1, pick(false)) + operate(1, 1, (x, y) => { x + y })
-						`,
+					operate(3, 1, pick(true)) + operate(5, 1, pick(false)) + operate(1, 1, (x, y) => { x + y })
+				`,
 		err: nil,
 	}, {
 		name: "compile euler2 without errors",
 		program: `
-							agg = (p2, p1, u, sum) => {
-								cur = p1 + p2
-								if (cur > u) sum else {
-									nsum = if (cur % 2 == 0) sum + cur else sum
-									agg(p1, cur, u, nsum)
-								}
-							}
-							agg(1, 1, 100, 0)
-						`,
+					agg = (p2, p1, u, sum) => {
+						cur = p1 + p2
+						if (cur > u) sum else {
+							nsum = if (cur % 2 == 0) sum + cur else sum
+							agg(p1, cur, u, nsum)
+						}
+					}
+					agg(1, 1, 100, 0)
+				`,
 		err: nil,
 	}, {
 		name: "compile nested function args without errors",
 		program: `
-						findLargestProd = (start, stop, cond) => {
-							agg = (b, e, le, ri, max, c) => {
-								if (le > e) {
-									agg(b, e, b, ri + 1, max, c)
-								} else if (ri > e) {
-									max
-								} else if (le * ri > max && c(le * ri)) {
-									agg(b, e, le + 1, ri, le * ri, c)
-								} else {
-									agg(b, e, le + 1, ri, max, c)
-								}
+					findLargestProd = (start, stop, cond) => {
+						agg = (b, e, le, ri, max, c) => {
+							if (le > e) {
+								agg(b, e, b, ri + 1, max, c)
+							} else if (ri > e) {
+								max
+							} else if (le * ri > max && c(le * ri)) {
+								agg(b, e, le + 1, ri, le * ri, c)
+							} else {
+								agg(b, e, le + 1, ri, max, c)
 							}
-							agg(start, stop, start, start, 0, cond)
 						}
+						agg(start, stop, start, start, 0, cond)
+					}
 
-						findLargestProd(10, 99, (x) => x % 2 == 0)
-					`,
+					findLargestProd(10, 99, (x) => x % 2 == 0)
+				`,
 		err: nil,
 	}, {
 		name: "compile sequential functions",
@@ -66,10 +66,10 @@ func TestCompile(t *testing.T) {
 	}, {
 		name: "cwork with unused values",
 		program: `
-						a = b
-						b = 3
-						3
-					`,
+					a = b
+					b = 3
+					3
+				`,
 		err: nil,
 	}, {
 		name: "compile values in right order",
