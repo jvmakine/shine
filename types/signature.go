f82840d9e463b376b7582ce55c5c200e46e590d4
Juho Makinen <jvmakine@gmail.com>
2020-06-06 13:39:21 +1000

    Added technical signature

--- types/signature.go
+++ types/signature.go
@@ -6,17 +6,21 @@ import (
 )
 
 type signctx struct {
+	deepSign        bool
 	varc            int
 	varm            map[*TypeVar]string
 	definingStructs map[string]bool
 }
 
-func (f Function) sign(ctx *signctx) string {
+func (f Function) sign(ctx *signctx, level int) string {
+	if level > 0 && !ctx.deepSign {
+		return "<fn>"
+	}
 	var sb strings.Builder
 	sb.WriteString("(")
 	if len(f) > 1 {
 		for i, p := range f {
-			sb.WriteString(sign(p, ctx))
+			sb.WriteString(sign(p, ctx, level+1))
 			if i < len(f)-2 {
 				sb.WriteString(",")
 			} else if i < len(f)-1 {
@@ -25,12 +29,15 @@ func (f Function) sign(ctx *signctx) string {
 		}
 	} else {
 		sb.WriteString(")=>")
-		sb.WriteString(sign(f[0], ctx))
+		sb.WriteString(sign(f[0], ctx, level+1))
 	}
 	return sb.String()
 }
 
-func (s Structure) sign(ctx *signctx) string {
+func (s Structure) sign(ctx *signctx, level int) string {
+	if level > 0 && !ctx.deepSign {
+		return "<st>"
+	}
 	if ctx.definingStructs[s.Name] {
 		return s.Name
 	}
@@ -45,7 +52,7 @@ func (s Structure) sign(ctx *signctx) string {
 	for i, p := range s.Fields {
 		sb.WriteString(p.Name)
 		sb.WriteString(":")
-		sb.WriteString(sign(p.Type, ctx))
+		sb.WriteString(sign(p.Type, ctx, level+1))
 		if i < len(s.Fields)-1 {
 			sb.WriteString(",")
 		}
@@ -54,7 +61,7 @@ func (s Structure) sign(ctx *signctx) string {
 	return sb.String()
 }
 
-func sign(t Type, ctx *signctx) string {
+func sign(t Type, ctx *signctx, level int) string {
 	if t.IsPrimitive() {
 		return *t.Primitive
 	}
@@ -78,10 +85,10 @@ func sign(t Type, ctx *signctx) string {
 		return ctx.varm[t.Variable]
 	}
 	if t.IsFunction() {
-		return t.Function.sign(ctx)
+		return t.Function.sign(ctx, level)
 	}
 	if t.IsStructure() {
-		return t.Structure.sign(ctx)
+		return t.Structure.sign(ctx, level)
 	}
 	if t.IsNamed() {
 		return *t.Named
@@ -95,5 +102,16 @@ func sign(t Type, ctx *signctx) string {
 func (t Type) Signature() string {
 	varm := map[*TypeVar]string{}
 	ds := map[string]bool{}
-	return sign(t, &signctx{varc: 0, varm: varm, definingStructs: ds})
+	ctx := signctx{varc: 0, varm: varm, definingStructs: ds, deepSign: true}
+	return sign(t, &ctx, 0)
+}
+
+// technical signature that does not differentiate between
+// structure or function types. Used to ientify actual generated
+// functions
+func (t Type) TSignature() string {
+	varm := map[*TypeVar]string{}
+	ds := map[string]bool{}
+	ctx := signctx{varc: 0, varm: varm, definingStructs: ds, deepSign: false}
+	return sign(t, &ctx, 0)
 }
