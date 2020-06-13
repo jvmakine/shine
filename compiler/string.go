0139a206674cf6d19fb12912e0d65f406bc54d92
Juho Makinen <jvmakine@gmail.com>
2020-06-13 16:40:02 +1000

    Refactoring

--- compiler/string.go
+++ compiler/string.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 	"unicode/utf16"
 
+	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
@@ -16,9 +17,9 @@ func (c *context) makeStringRefRoot(str string) value.Value {
 	if c.global.strings[str] != nil {
 		return c.global.strings[str]
 	}
+	stringID := "string%" + strconv.Itoa(len(c.global.strings))
 	leafType := types.NewStruct(types.I32, types.NewArray(PV_BRANCH, types.I16))
 	nodeType := types.NewStruct(types.I32, types.NewArray(PV_BRANCH, types.I8Ptr))
-	headType := types.NewStruct(types.I8, types.I32, types.I32, types.I8Ptr)
 
 	encoded := utf16.Encode([]rune(str))
 	nodes := []constant.Constant{}
@@ -51,30 +52,13 @@ func (c *context) makeStringRefRoot(str string) value.Value {
 	depth = 0
 	arrayType := types.NewArray(uint64(len(nodes)), leafType)
 	array := constant.NewArray(nil, nodes...)
-	id := "string%" + strconv.Itoa(depth) + "%" + strconv.Itoa(len(c.global.strings))
+	id := stringID + "%" + strconv.Itoa(depth)
 	gd := c.global.Module.NewGlobalDef(id, array)
 
 	if depth <= 1 {
 		depth++
-		i := 0
-		n := 0
-		cs := make([]constant.Constant, PV_BRANCH)
-		for i < PV_BRANCH {
-			if n < len(nodes) {
-				ptr := constant.NewGetElementPtr(arrayType, gd, constant.NewInt(types.I32, int64(n)))
-				cs[i] = constant.NewBitCast(ptr, types.I8Ptr)
-			} else {
-				cs[i] = constant.NewNull(types.I8Ptr)
-			}
-			n++
-			i++
-		}
-		arr := constant.NewArray(nil, cs...)
-		l := constant.NewStruct(nodeType, zero, arr)
+		gd = makePVNode(c, arrayType, gd, len(nodes), stringID+"%"+strconv.Itoa(depth))
 		arrayType = types.NewArray(1, nodeType)
-		array = constant.NewArray(nil, l)
-		id = "string%" + strconv.Itoa(depth) + "%" + strconv.Itoa(len(c.global.strings))
-		gd = c.global.Module.NewGlobalDef(id, array)
 	} else {
 		for len(nodes) > 1 {
 			depth++
@@ -100,23 +84,46 @@ func (c *context) makeStringRefRoot(str string) value.Value {
 			nodes = new
 			arrayType = types.NewArray(uint64(len(nodes)), nodeType)
 			array = constant.NewArray(nil, nodes...)
-			id = "string%" + strconv.Itoa(depth) + "%" + strconv.Itoa(len(c.global.strings))
+			id = stringID + "%" + strconv.Itoa(depth)
 			gd = c.global.Module.NewGlobalDef(id, array)
 		}
 	}
 	ptr := constant.NewGetElementPtr(arrayType, gd, constant.NewInt(types.I32, 0))
+	res := makePVHead(c, ptr, len(encoded), stringID)
+	c.global.strings[str] = res
+	return res
+}
 
+func makePVNode(c *context, arrayType types.Type, gd constant.Constant, nodes int, id string) *ir.Global {
+	nodeType := types.NewStruct(types.I32, types.NewArray(PV_BRANCH, types.I8Ptr))
+	zero := constant.NewInt(types.I32, 0)
+	cs := make([]constant.Constant, PV_BRANCH)
+	i := 0
+	for i < PV_BRANCH {
+		if i < nodes {
+			ptr := constant.NewGetElementPtr(arrayType, gd, constant.NewInt(types.I32, int64(i)))
+			cs[i] = constant.NewBitCast(ptr, types.I8Ptr)
+		} else {
+			cs[i] = constant.NewNull(types.I8Ptr)
+		}
+		i++
+	}
+	arr := constant.NewArray(nil, cs...)
+	l := constant.NewStruct(nodeType, zero, arr)
+	array := constant.NewArray(nil, l)
+	return c.global.Module.NewGlobalDef(id, array)
+}
+
+func makePVHead(c *context, node constant.Constant, length int, id string) constant.Constant {
+	headType := types.NewStruct(types.I8, types.I32, types.I32, types.I8Ptr)
 	head := constant.NewStruct(
 		headType,
 		constant.NewInt(types.I8, 3),
 		constant.NewInt(types.I32, 0),
-		constant.NewInt(types.I32, int64(len(encoded))),
-		constant.NewBitCast(ptr, types.I8Ptr))
+		constant.NewInt(types.I32, int64(length)),
+		constant.NewBitCast(node, types.I8Ptr))
 
-	id = "string%" + strconv.Itoa(len(c.global.strings))
-	gd = c.global.Module.NewGlobalDef(id, head)
+	gd := c.global.Module.NewGlobalDef(id, head)
 
-	res := constant.NewBitCast(gd, types.I8Ptr)
-	c.global.strings[str] = res
-	return res
+	return constant.NewBitCast(gd, types.I8Ptr)
 }
