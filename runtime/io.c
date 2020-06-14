387706b72a5ef5508d6a27193701b12a21e779bf
Juho Makinen <jvmakine@gmail.com>
2020-06-14 18:39:33 +1000

    UTF8 output

--- runtime/io.c
+++ runtime/io.c
@@ -22,8 +22,29 @@ void print_bool(int8_t b) {
 void print_string(PVHead *str) {
     for (int i = 0; i < str->size; ++i) {
         uint16_t c = pvector_get_uint16(str, i);
+        uint32_t code = c;
+        if (code > 0xd7ff && code < 0xe000) {
+            // TODO: large code points
+        }
+        if (code <= 0x7f) {
+            printf("%c", c);
+        } else if (code <= 0x7ff) {
+            uint8_t b1 = 0b11000000 + ((code >> 6) & 0b00011111);
+            uint8_t b2 = 0b10000000 + (code & 0b00111111);
+            printf("%c%c", b1, b2);
+        } else if (code <= 0xffff) {
+            uint8_t b1 = 0b11100000 + ((code >> 12) & 0b00001111);
+            uint8_t b2 = 0b10000000 + ((code >> 6) & 0b00111111);
+            uint8_t b3 = 0b10000000 + (code & 0b00111111);
+            printf("%c%c%c", b1, b2, b3);
+        } else {
+            uint8_t b1 = 0b11110000 + ((code >> 18) & 0b00000111);
+            uint8_t b2 = 0b10000000 + ((code >> 12) & 0b00111111);
+            uint8_t b3 = 0b10000000 + ((code >> 6) & 0b00111111);
+            uint8_t b4 = 0b10000000 + (code & 0b00111111);
+            printf("%c%c%c%c", b1, b2, b3, b4);
+        }
         // TODO: UTF-8 conversion
-        printf("%c", c);
     }
     printf("\n");
 }
